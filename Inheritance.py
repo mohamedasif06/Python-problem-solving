840750659dfd24a2113e230e31b111223ebd9496
Mohamed Asif AR <mohamedasif3417@gmail.com>
2026-06-23 20:33:54 +0530

    Inheritance example

--- Inheritance.py
+++ Inheritance.py
@@ -1,18 +1,36 @@
 class Animal:
-    def __init__(self,name):
+    def __init__(self,species,name):
+        self.species = species
         self.name = name
-        self.is_pet = True
+
+class Bird(Animal):
+    def __init__(self,species,name):
+        super().__init__(species,name)
+    
+    def fly(self):
+        print("The bird is flying.")
 
 class Dog(Animal):
-    def __init__(self,name,breed):
-        super().__init__(name) #pass the name to parent class
-        self.breed = breed
-    
-    def describe(self):
-        print(f"The {self.name} is a {self.breed}.")
+    def __init__(self, species, name):
+        super().__init__(species, name) #pass the name and species to parent class
+    def bark(self):
+        print("Dog Barking!")
 
-dog1 = Dog(name = "Jimmy",breed = "Labrodor")
-dog2 = Dog(name = "Tim",breed = "Doberman")
-dog1.describe()
-dog2.describe()
+bird1 = Bird(species = "Pineapple Conure",name = "Zoro")
+dog1 = Dog(species="Beagle",name="Tim")
+flag = False
+while flag != True:
+    data = input("Enter the animal Dog/Bird: ").lower()
+    if data == "dog":
+        print(f"The name of the dog is {dog1.name} which belongs to {dog1.species} species.")
+        dog1.bark()
+        flag = True
+    elif data == "bird":
+        print(f"The name of the bird is {bird1.name} which belongs to {bird1.species} species.")
+        bird1.fly()
+        flag = True
+    else:
+        print(f"The given input {data} is invalid.")
+        flag = False
+        print("\nRe-Enter.....")
 
