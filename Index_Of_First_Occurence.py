2a9fb3235fce3d28da51e38dff954ae0d7584a0a
Mohamed Asif AR <mohamedasif3417@gmail.com>
2026-08-19 19:41:47 +0530

    Update Index_Of_First_Occurence.py

--- Index_Of_First_Occurence.py
+++ Index_Of_First_Occurence.py
@@ -1,7 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         n = len(needle)
-        for i in range(n):
+        for i in range(len(haystack) - n+1): #we want to check every position where needle can possibly fit inside haystack.
             if haystack[i:i+n] == needle:
                 return i
         return -1
